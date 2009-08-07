6516aba4767736955b548aea5eb5a575a2980a40
Lucas Menge <lmmenge@gmail.com>
2009-08-07 16:15:04 -0300

    site funcionando

--- scripts.js
+++ scripts.js
@@ -3,6 +3,6 @@ function showScreenshot(name)
   var screenView = document.getElementById("screenshotView");
   if(screenView != null)
   {
-    screenView.src = "/images/content/"+name;
+    screenView.src = "images/content/"+name;
   }
 }
