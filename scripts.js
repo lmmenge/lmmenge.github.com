5eaec67d4856b0c3da086bb0a1146d58963e170f
Lucas Menge <lmmenge@gmail.com>
2010-01-27 09:22:19 -0300

    BeatCoach

--- scripts.js
+++ scripts.js
@@ -4,5 +4,7 @@ function showScreenshot(name)
   if(screenView != null)
   {
     screenView.src = "images/content/"+name;
+    //if(document.getElementByName("iPhone") != null)
+      location.href = "#iPhone";
   }
 }
