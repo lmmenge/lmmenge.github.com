e840ebaf9cd8e5f36a330a166f152a5ac9d6b7cf
Lucas Menge <lmmenge@gmail.com>
2012-02-20 22:13:33 -0300

    Cartalog

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ function showScreenshot(name)
   var screenView = document.getElementById("screenshotView");
   if(screenView != null)
   {
-    screenView.src = "images/content/"+name;
+    screenView.src = "/images/content/"+name;
     //if(document.getElementByName("iPhone") != null)
       location.href = "#iPhone";
   }
